c21125bad8445a3463fbc800d5014c468c8452dd
aidanautep90 <60244578+aidanautep90@users.noreply.github.com>
2021-03-20 03:01:47 +0500

    Create Str_data_type.py

---
+++ Str_data_type.py
@@ -0,0 +1,19 @@
+#myString = "This is a string"
+#print(myString)
+#print type(myString)
+
+#a="water"
+#b="fall"
+#c=a+b
+#print(c)
+
+#name = input("Whats your name?")
+#print(name)
+#print (str(name), " is a beautiful natute " + str(type(c)))
+#print(name)
+
+name = input("Whats your name?")
+color = input("What's your favorite color? ")
+animal = input("What is your favorite animal? ")
+#print("{}, you like a {} {}!".format(name,color,animal))
+print(f"{name}, you like a {color} {animal}")
