8ebeb1e483fe855d6f1ebd06e70bbb774383a61b
aidanautep90 <60244578+aidanautep90@users.noreply.github.com>
2021-03-20 03:29:32 +0500

    Create categorize_values.py

---
+++ categorize_values.py
@@ -0,0 +1,4 @@
+myMixedBagList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+for item in myMixedBagList:
+    #print("{} is of data type {}".format(item,type(item)))
+    print(f"{item} data type of ", type(item))
